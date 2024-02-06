98ea97fb5d380b2bc19a5f4a8484a038da2f171f
Vojtech Benes <vojtech.benes@carvago.com>
2024-02-06 13:30:03 +0100

    button size and variant extension, add hover effect

--- src/app/ui/Card.tsx
+++ src/app/ui/Card.tsx
@@ -1,6 +1,7 @@
+'use client'
 import Image from 'next/image'
 import { imageSizeEnum } from '@/app/lib/types'
-import { Button } from '@/ui/Button/Button'
+import { Button } from '@/components/Button/Button'
 
 export type CardProps = {
   imageSize: imageSizeEnum;
@@ -11,40 +12,31 @@ export type CardProps = {
   description: string;
 };
 
-export const Card = ({
+export function Card({
   imageSize,
   imageAlt,
   imageSrc,
   title,
   titleEng,
   description,
-}: CardProps) => {
+}: CardProps) {
   const imageWidth = imageSize === 'small' ? 100 : 348
   const imageHeight = imageSize === 'small' ? 100 : 196
 
   return (
-    <div
-      className="border border-l-stone-400 hover:border-gray-300 shadow rounded-lg"
-      style={{ width: '348px', backgroundColor: '#444' }}
-    >
+    <div className="shadow hover:shadow-2xl rounded-lg w-[348px] bg-gray-800">
       <Image
         className={'rounded-lg'}
         src={imageSrc}
         alt={imageAlt}
         width={imageWidth}
         height={imageHeight}
+        priority={false}
       />
       <div className="p-5">
-        <h3 className="text-xl mb-3 font-semibold" style={{ color: '#4caf50' }}>
-          {title}
-        </h3>
-        <p className="mb-4" style={{ color: '#ddd' }}>
-          {description}
-        </p>
-        <a href={''} className="py-1.5 px-3 bg-blue-500 text-white rounded">
-          {titleEng}
-        </a>
-        <Button title={'Button'} />
+        <h3 className="text-xl mb-3 font-semibold text-green-500">{title}</h3>
+        <p className="mb-4 text-gray-200">{description}</p>
+        <Button title={titleEng} onClick={() => console.log('clicked')} />
       </div>
     </div>
   )
---
+++ src/app/ui/HeroSection.tsx
@@ -0,0 +1,3 @@
+export const HeroSection = () => {
+  return <div className={'w-full bg-blue-500 h-80 m-8'}></div>
+}
